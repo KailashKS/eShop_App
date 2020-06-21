0fd1128225c8efc763e557cc3af7d13777606c1c
Kailash K S <karthikkailash6@gmail.com>
2020-06-21 20:46:47 +0530

    4th version

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 //Kailash K S
 import 'package:flutter/material.dart';
-import 'package:newitempage/home/itemlist.dart';
-import 'database.dart';
+import 'model/user.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: ItemPage(),
-      //home: ItemPage(),
-    ));
+//void main() {
+////  runApp(MaterialApp(
+////    home: ItemPage(),
+////      //home: ItemPage(),
+////    ));
+////  }
+
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        //home: Forgot(),
+        home: Wrapper(),
+        //home: Retrieve1(),
+      ),
+    );
   }
-  
-
+}
